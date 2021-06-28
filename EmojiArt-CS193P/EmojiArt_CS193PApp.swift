13d89489cac9174918e150f05c3f28cd5a4ed331
Matthew Folbigg <matthew.folbigg@icloud.com>
2021-06-28 17:54:24 +0100

    Initial Commit

---
+++ EmojiArt-CS193P/EmojiArt_CS193PApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArt_CS193PApp.swift
+//  EmojiArt-CS193P
+//
+//  Created by Matthew Folbigg on 28/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArt_CS193PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
